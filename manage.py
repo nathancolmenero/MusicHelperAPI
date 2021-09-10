4fae5981c803bd9f24beaf365c36c3d37842b33e
Nathan Colmenero <nathancolmenero@gmail.com>
2021-09-10 12:48:06 -0700

    Swedish push

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks.""" 
-    """Hello"""
+    """Hej"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_chords.settings')
     try:
         from django.core.management import execute_from_command_line
