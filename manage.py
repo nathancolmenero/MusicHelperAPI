e7e924f41fc0fe9702e035042ef4ffde860f23cd
Nathan Colmenero <nathancolmenero@gmail.com>
2021-09-10 13:48:15 -0700

    No second comment

--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks.""" 
-    """JAMBO"""
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_chords.settings')
     try:
         from django.core.management import execute_from_command_line
