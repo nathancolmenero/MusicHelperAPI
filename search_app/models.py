bfb0b82ee88599ac955c90e55c0086aa971d6af0
Nathan Colmenero <nathancolmenero@gmail.com>
2021-09-29 16:41:00 +0200

    models.py update 2

--- search_app/models.py
+++ search_app/models.py
@@ -1,12 +1,16 @@
+from django.core import validators
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Chords(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
-    genre = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    genre = models.CharField(null=True, max_length=50)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    legend = models.BooleanField(default=False)
 
-    def __str__():
-        return f"{self.title} ({self.artist}) ({self.genre}) ({self.rating})"
+    def __str__(self):
+        return f"{self.title} ({self.artist}) {self.genre} {self.rating}"
